0a9c85d7512dc5310d27fde117836854c5e72dcd
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-25 16:05:57 +0100

    feat(firestore_helper.py): create function to convert firestore document into a dict
    to process the firestore document, the document needs to be in a format that is easily manupilated in python. a dict was chosen for this

--- source/helpers/firestore_functions.py
+++ source/helpers/firestore_functions.py
@@ -1,1 +1,16 @@
 from google.cloud import firestore
+
+def document_to_dict(doc):
+    """This function creates a dictionary object containing details of a document
+
+    Args:
+        doc (firestore document): The firestore document object
+
+    Returns:
+        Dict: a dictionary containing a firestore document and metadata
+    """
+    if not doc.exists:
+        return None
+    doc_dict = doc.to_dict()
+    doc_dict['id'] = doc.id
+    return doc_dict
