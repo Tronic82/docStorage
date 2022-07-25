a0bd9956f4c53e1fe9c6797bd7e8c9e1f44527a1
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-25 16:07:48 +0100

    feat(firestore_functions.py): read a firestore document
    this fucntion takes in an id and reads firestore to retreive the document. this uses the doccument_to_dict function to return a dict

--- source/helpers/firestore_functions.py
+++ source/helpers/firestore_functions.py
@@ -14,3 +14,19 @@ def document_to_dict(doc):
     doc_dict = doc.to_dict()
     doc_dict['id'] = doc.id
     return doc_dict
+
+def read(pdfdoc_id):
+    """This function reads firestore for the specified document
+
+    Args:
+        pdfdoc_id (Any): the ID of the document to read
+
+    Returns:
+        Dict: a dictionary of the document and its metadata
+    """
+    
+    db_client = firestore.Client()
+    pdfdoc_ref = db_client.collection(u'pdfdoc').document(pdfdoc_id)
+    snapshot = pdfdoc_ref.get()
+   
+    return document_to_dict(snapshot)
