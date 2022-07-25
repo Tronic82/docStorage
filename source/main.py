2291d26d276c32df39307abb05c648eb3a1ccb5b
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-25 17:45:01 +0100

    feat(main.py): add view, add and list flask routes
    add routes to render the html templates

--- source/main.py
+++ source/main.py
@@ -1,6 +1,6 @@
 import os
-from helpers import storage_functions
-from flask import current_app, Flask
+from helpers import storage_functions, firestore_functions
+from flask import current_app, Flask, request, render_template, render_template, url_for, redirect
 import logging
 import google.cloud.logging
 
@@ -50,3 +50,27 @@ logging.basicConfig(level=logging.INFO)
 logging_client = google.cloud.logging.Client()
 # Attaches a Google logging handler to the root logger
 logging_client.setup_logging(logging.INFO)
+
+@app.route('/')
+def list():
+    # determine if program needs to list from a specific file
+    start_after = request.args.get('start_after', None)
+    docs, last_title = firestore_functions.next_page(start_after=start_after)
+
+    return render_template('list.html', docs=docs, last_title=last_title)
+
+@app.route('/docs/<doc_id>')
+def view(doc_id):
+    doc = firestore_functions.read(doc_id)
+    return render_template('view.html', doc=doc)
+
+@app.route('/docs/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+
+        doc = firestore_functions.create(data)
+
+        return redirect(url_for('.view', doc_id=doc['id']))
+
+    return render_template('form.html', action='Add', doc={})
