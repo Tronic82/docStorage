2b14251c24728de30e1c443b25fe9cd607c1d8cf
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-25 16:20:18 +0100

    feat(storage_functions.py): create function to create unique filename for each file uploaded to GCS
    as users will be uploading files to GCS, unique filenames are needed to avoid conflicts or accidental overriding of an existing file

--- source/helpers/storage_functions.py
+++ source/helpers/storage_functions.py
@@ -1,5 +1,8 @@
 import os
+import datetime
+import hashlib
 from werkzeug.exceptions import BadRequest
+from werkzeug.utils import secure_filename
 
 def _check_extension(filename, allowed_extensions):
     """This Function checks whether the filename conforms to the allowed extensions
@@ -15,3 +18,24 @@ def _check_extension(filename, allowed_extensions):
     if (ext.replace('.', '').lower() not in allowed_extensions):
         raise BadRequest(
             f'{filename} has an invalid name or extension')
+
+def _safe_filename(filename):
+    """
+    Generates a unique filename that is unlikely to collide with existing
+    objects in Google Cloud Storage.
+
+    ``filename.ext`` is transformed into ``filenameYYYYMMDDHHMMSS.ext`` 
+    filename is then ran through sha256 to generate a unique filename for GCS Bucket 
+
+    Args:
+        filename (String): The name of the file
+
+    Returns:
+        String: sha256 hexdigest of the new computed file name
+    """
+    filename = secure_filename(filename)
+    date = datetime.datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    basename, extension = filename.rsplit('.', 1)
+    tempname = f"{basename}{date}{extension}".encode('utf-8')
+    hashname = hashlib.sha256(tempname).hexdigest()
+    return hashname
