9fad3426990e6c8c6b2adf0afdc2fa894d9a3732
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-25 16:23:34 +0100

    feat(storage_functions.py): create function to upload file to gcs
    create a new function that uses the pre defined functions to upload a uniquely named file to GCS. this also provides a public URL for the user to retrieve the uploaded file

--- source/helpers/storage_functions.py
+++ source/helpers/storage_functions.py
@@ -3,6 +3,8 @@ import datetime
 import hashlib
 from werkzeug.exceptions import BadRequest
 from werkzeug.utils import secure_filename
+from flask import current_app
+from google.cloud import storage
 
 def _check_extension(filename, allowed_extensions):
     """This Function checks whether the filename conforms to the allowed extensions
@@ -39,3 +41,39 @@ def _safe_filename(filename):
     tempname = f"{basename}{date}{extension}".encode('utf-8')
     hashname = hashlib.sha256(tempname).hexdigest()
     return hashname
+
+def upload_file(file_stream, filename, content_type):
+    """
+    Uploads a file to a given Cloud Storage bucket and returns the public url
+    to the new object.
+
+    Args:
+        file_stream (String): The filestream being read 
+        filename (String): name of the file to be uploaded
+        content_type (): the content type the file
+
+    Returns:
+        string: url to the uploaded file
+    """
+    # as only PDFs are allowed, we need to check the user isnt trying to upload any other file
+    _check_extension(filename, current_app.config['ALLOWED_EXTENSIONS'])
+    filename = _safe_filename(filename)
+
+    bucketname = os.getenv('GOOGLE_STORAGE_BUCKET') or f"{os.getenv( 'GOOGLE_CLOUD_PROJECT')}-file-bucket"
+
+    gcs_client = storage.Client()
+    gcs_bucket = gcs_client.bucket(bucketname)
+    blob = gcs_bucket.blob(filename)
+
+    blob.upload_from_string(
+        file_stream,
+        content_type=content_type)
+
+    # provide a url to the storage object
+    url = blob.public_url
+
+    # check to see if string needs decoding before being returned
+    if isinstance(url, bytes):
+        url = url.decode('utf-8')
+
+    return url
