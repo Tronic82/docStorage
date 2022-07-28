5f09c5e4fe37a6a2e0f795ed1019f8308d86d5de
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-28 19:49:19 +0100

    test(test_main.py): test the add functionality when making a post request and proividing data. and redirect to the list page

--- source/tests/functional/test_main.py
+++ source/tests/functional/test_main.py
@@ -37,3 +37,26 @@ def test_add_get(new_mock_flask_app):
     assert b"Welcome to doc Storage" in response.data
     assert b"Add Document" in response.data
     assert b"Save" in response.data
+
+def test_add_post(new_mock_flask_app):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/docs/add' page is requested (POST) and data is provided
+    THEN check that there is a 200 response and the form is shown
+    """
+    data = {
+        "title" :"test add",
+        "author":"test author",
+        "authorid":"testauthor_id",
+        "publishedDate": "2022-01-01",
+        "description": "test description"
+    }
+
+    response = new_mock_flask_app.post("/docs/add", follow_redirects=True, data=data )
+    
+    assert response.status_code == 200
+    assert f"{data['author']}".encode() in response.data
+    assert f"{data['title']}".encode() in response.data
+    assert f"{data['authorid']}".encode() in response.data
+    assert f"{data['publishedDate']}".encode() in response.data
+    assert f"{data['description']}".encode() in response.data
