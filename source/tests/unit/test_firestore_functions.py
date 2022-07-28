a36671937fe17e32887737a86b9e4b54d8afe041
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-28 17:42:26 +0100

    test(test_firestore_functions.py): test the document_to_dict function when provided with a valid document
    this tests the second outcome of the function. When a document id proivided, this function should successfuly convert it into a dictionary

--- source/tests/unit/test_firestore_functions.py
+++ source/tests/unit/test_firestore_functions.py
@@ -1,5 +1,6 @@
 from helpers import firestore_functions
 
+# the document_to_dict function has 2 outcomes, one if the docmunent provcided is empty and one with a valid document
 def test_document_to_dict_empty(new_mock_firestore):
     """ 
     GIVEN a firestore database instance
@@ -9,3 +10,17 @@ def test_document_to_dict_empty(new_mock_firestore):
     # get an empty document
     doc = new_mock_firestore.collection(u'fs_unit_test').document("test_doc")
     assert firestore_functions.document_to_dict(doc.get()) == None
+
+def test_document_to_dict(new_mock_firestore):
+    """ 
+    GIVEN a firestore database instance
+    WHEN a firestore document is returned
+    THEN check the function returns a dictionary
+    """
+    #create a mock doc
+    doc = new_mock_firestore.collection(u'fs_unit_test').document("test_doc")
+    doc.set({
+        'first': 'Ada',
+        'last': 'Lovelace'
+    })
+    assert isinstance(firestore_functions.document_to_dict(doc.get()),dict)
