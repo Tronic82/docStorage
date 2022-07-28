4dacca94cf8dea56718511daddcc3b2682a8ce74
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-28 18:08:11 +0100

    test(test_firestore_functions.py): test the update firestore document function
    the test aims to check wether the function updates an existing document with the given data

--- source/tests/unit/test_firestore_functions.py
+++ source/tests/unit/test_firestore_functions.py
@@ -45,8 +45,8 @@ def test_read_doc(new_mock_firestore):
     #create a mock doc
     doc = new_mock_firestore.collection(u'fs_unit_test').document("test_doc")
     doc.set({
-        'first': 'Ada',
-        'last': 'Lovelace'
+        'first': 'Ada_read',
+        'last': 'Lovelace_read'
     })
     assert isinstance(firestore_functions.read(client=new_mock_firestore, pdfdoc_id="test_doc", collection_name="fs_unit_test"),dict)
 
@@ -57,8 +57,8 @@ def test_create(new_mock_firestore):
     THEN check the function creates a firestore document
     """
     data = {
-        'first': 'Ada',
-        'last': 'Lovelace'
+        'first': 'Ada_create',
+        'last': 'Lovelace_create'
     }
 
     created_doc = firestore_functions.create(client=new_mock_firestore, data=data, collection_name="fs_unit_test")
@@ -66,6 +66,32 @@ def test_create(new_mock_firestore):
     # the idea here is that if the doc is created, firestore creates an ID for it. as we dont have access 
     # to this ID, we set the ID here after the first assertion to ensure it is created first. When the test runs, 
     # it will fail if anyone of the values dont match up
-    # and since IDs are relatively unique, there is no chance of a previously created instance having the same ID and the same values
+    # and since IDs are relatively unique, there is low chance of a previously created instance having the same ID and the same values
     data['id'] = created_doc['id']
     assert created_doc == data
+
+def test_update(new_mock_firestore):
+    """ 
+    GIVEN a firestore database instance
+    WHEN data is provided and ID is provided
+    THEN check the function updates a firestore document
+    """
+    #create an existing firebase document
+    #create a mock doc
+    doc = new_mock_firestore.collection(u'fs_unit_test').document("test_doc_update")
+    doc.set({
+        'first': 'Ada_update',
+        'last': 'Lovelace_update'
+    })
+
+    created_doc = new_mock_firestore.collection(u'fs_unit_test').document("test_doc_update").get().to_dict()
+    assert isinstance(created_doc,dict)
+    #ensure the data is actually created
+    assert created_doc == {
+        'first': 'Ada_update',
+        'last': 'Lovelace_update'
+        }
+    data = {
+        'nickname': 'happy feet'
+    }
+    assert firestore_functions.update(client=new_mock_firestore, data=data, pdfdoc_id="test_doc_update", collection_name="fs_unit_test") == {'id': 'test_doc_update','nickname': 'happy feet'}
