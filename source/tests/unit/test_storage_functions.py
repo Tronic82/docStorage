0c363ff23d385095fc5f0ed736b962fd7805f0b3
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-28 18:20:17 +0100

    test(test_storage_functions.py): test the function that checks if the uploaded file has a valid extension
    the function should raise a bad request whenever the file being uploaded is not of the valid extensions. similarly we want to check whether uploading a file with a valid extenstion will be valid

--- source/tests/unit/test_storage_functions.py
+++ source/tests/unit/test_storage_functions.py
@@ -2,8 +2,9 @@ import io
 from google.cloud import storage
 from unittest import mock
 from werkzeug.datastructures import FileStorage
-
+from werkzeug.exceptions import BadRequest
 from helpers import storage_functions
+import pytest
 
 def test_upload_file(new_mock_flask_app):
     """ 
@@ -27,3 +28,23 @@ def test_upload_file(new_mock_flask_app):
     # run test
     storage_functions.upload_file(storage_client,test_filestream.read(),test_filestream.filename,bucket=storage_client.bucket("test-bucket").return_value, content_type=test_filestream.content_type)
     storage_client.bucket("test-bucket").blob("test-filename").upload_from_string.assert_called_once_with(b"this is a test", content_type=test_filestream.content_type)
+
+def test_check_extension(new_mock_flask_app):
+    """ 
+    GIVEN a file
+    WHEN a file is to be uploaded
+    THEN check a bad request exception is raised when the extension is invalid
+    """
+    # ALLOWED_EXTENSIONS=set(['png', 'jpg', 'jpeg', 'gif', 'pdf', 'txt']) found in fixtures config
+    correct_filename= "test.pdf"
+    incorrect_filename = "test.exe"
+
+    #assert a bad request is raised for incorrect file name
+    with pytest.raises(BadRequest):
+        storage_functions._check_extension(incorrect_filename,new_mock_flask_app.application.config.get('ALLOWED_EXTENSIONS'))
+
+    #assert no exception if using correct file name
+    try:
+        storage_functions._check_extension(correct_filename,new_mock_flask_app.application.config.get('ALLOWED_EXTENSIONS'))
+    except BadRequest:
+        assert False, f"{correct_filename} has an invalid name or extension"
