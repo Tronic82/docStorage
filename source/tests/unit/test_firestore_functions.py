4b7e9b8264b3c0615d99482097f14a8a505d6404
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-28 17:56:12 +0100

    test(test_firestore_functions.py): test wether the function can correctly create a firestore document given the data
    we test wether the function can create the firestore isntance with the right data as well as return the dictionary

--- source/tests/unit/test_firestore_functions.py
+++ source/tests/unit/test_firestore_functions.py
@@ -49,3 +49,23 @@ def test_read_doc(new_mock_firestore):
         'last': 'Lovelace'
     })
     assert isinstance(firestore_functions.read(client=new_mock_firestore, pdfdoc_id="test_doc", collection_name="fs_unit_test"),dict)
+
+def test_create(new_mock_firestore):
+    """ 
+    GIVEN a firestore database instance
+    WHEN data is provided and no ID is provided
+    THEN check the function creates a firestore document
+    """
+    data = {
+        'first': 'Ada',
+        'last': 'Lovelace'
+    }
+
+    created_doc = firestore_functions.create(client=new_mock_firestore, data=data, collection_name="fs_unit_test")
+    assert isinstance(created_doc,dict)
+    # the idea here is that if the doc is created, firestore creates an ID for it. as we dont have access 
+    # to this ID, we set the ID here after the first assertion to ensure it is created first. When the test runs, 
+    # it will fail if anyone of the values dont match up
+    # and since IDs are relatively unique, there is no chance of a previously created instance having the same ID and the same values
+    data['id'] = created_doc['id']
+    assert created_doc == data
