b367c5f85aa9e3940b3ab1687d8111e73e895c94
Tronic82 <61363124+Tronic82@users.noreply.github.com>
2022-07-28 17:44:50 +0100

    test(test_firestore_functions.py): test the firestore read function for when teh document exists or not
    the read function has two outcomes, when the document exists, it should reurn a dictionary with its details. when the document doesnt exist, the function should return none

--- source/tests/unit/test_firestore_functions.py
+++ source/tests/unit/test_firestore_functions.py
@@ -24,3 +24,28 @@ def test_document_to_dict(new_mock_firestore):
         'last': 'Lovelace'
     })
     assert isinstance(firestore_functions.document_to_dict(doc.get()),dict)
+
+# the read function has 2 outcomes, where the document doesnt exist and where it does
+def test_read_no_doc(new_mock_firestore):
+    """ 
+    GIVEN a firestore database instance
+    WHEN a firestore document id doesnt exist
+    THEN check the function returns NONE
+    """
+
+    # try to read a document that doesnt exist
+    assert firestore_functions.read(client=new_mock_firestore, pdfdoc_id="test_doc_no_exist", collection_name="fs_unit_test") == None
+
+def test_read_doc(new_mock_firestore):
+    """ 
+    GIVEN a firestore database instance
+    WHEN a firestore document id exists
+    THEN check the function returns a dictionary
+    """
+    #create a mock doc
+    doc = new_mock_firestore.collection(u'fs_unit_test').document("test_doc")
+    doc.set({
+        'first': 'Ada',
+        'last': 'Lovelace'
+    })
+    assert isinstance(firestore_functions.read(client=new_mock_firestore, pdfdoc_id="test_doc", collection_name="fs_unit_test"),dict)
